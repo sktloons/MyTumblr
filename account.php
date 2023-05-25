9bb3733429e1f195ce92e2ddd6ce8f5ce0fc5b1d
VelascoMarlon <sktloons02@gmail.com>
2023-05-25 14:21:54 +0800

    php login script added

--- account.php
+++ account.php
@@ -1,3 +1,35 @@
+<?php
+//start the session
+SESSION_START();
+
+//check if the session variables are set
+if (isset($_SESSION['ses_username'])===false){
+    header("Location: index.php?logfirst");
+
+
+} else if (isset($_REQUEST['logout'])===true){
+    session_destroy();
+    header("Location: index.php?logout");
+
+
+
+}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
 <!doctype html>
                         <html>
                             <head>
@@ -32,13 +64,17 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
+                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail">
+
+                        <a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Sign Out</a>
 
 
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">Ryan Clifford L. Perez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque PH
+                        <h4 class="mt-0 mb-0"> <?php echo $_SESSION['ses_fullname']; ?> </h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>
+
+                            <?php echo $_SESSION['ses_address']; ?>
                          </p>
                     </div>
                 </div>
--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+//start the session function
+SESSION_START();
+
+//create a pre defined username and password
+$acc_username = "Marlon123";
+$acc_password = "Velasco123";
+$acc_fullname = "Marlon Velasco";
+$acc_address = "Mahinhin, Boac, Marinduque";
+//check the current URL address for the redirections
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+
+
+//condition para malaman kung ang button ay pinindot
+if(isset($_REQUEST['login_button']) === true){
+	//get the username and password from the form and compare  to the predefined username and password
+	//kapag mali agad yung username
+		if($_REQUEST['form_username'] != $acc_username){
+			header("Location: ".$url_add."?notexist");
+		}	
+		//tama ang username pero mali ang password
+		else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password){
+			header("Location: ".$url_add."?wrongpass");
+		} 
+		//tama ang username at password
+		else if ($_REQUEST['form_username']==$acc_username && $_REQUEST['form_password'] == $acc_password){
+			header("Location: ".$url_add."?success");
+
+		//create a sesssion variables
+		$_SESSION['ses_username'] = $acc_username;
+		$_SESSION['ses_password']= $acc_password;
+		$_SESSION['ses_fullname']= $acc_fullname;
+		$_SESSION['ses_address']= $acc_address;
+
+
+		}//end of correct username and password
+
+}//end of login button
+
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,9 +52,7 @@
 
 
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
 	<link rel="stylesheet" href="css/style.css">
 
 	</head>
@@ -24,12 +67,51 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+
+
+
+
+
+
+						<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php
+		      			//this is the messaging
+		      			if (isset($_REQUEST['notexist']) === true){
+		      					echo "<div class='alert alert-danger' role='alert'> Username does not exist...
+		      						</div>";
+
+		      			} else if (isset($_REQUEST['wrongpass']) === true){
+		      					echo "<div class='alert alert-warning' role='alert'>	Incorrect Password...</div>";
+
+		      			} else if (isset($_REQUEST['success']) === true){
+		      					echo "<div class='alert alert-success' role='alert'>	Redirecting...</div>";
+		      					header ("Refresh: 5; url=account.php");
+		      			}	else if (isset($_REQUEST['logout'])===true){
+
+		      						echo "<div class='alert alert-info' role='alert'> Thank You...</div>";
+
+		      			}	else if (isset($_REQUEST['logfirst'])===true){
+		      						echo "<div class='alert alert-info' role='alert'> Please Login First.</div>";
+		      			} else if (isset($_SESSION['ses_username'])===true){
+		      							echo "<div class='alert alert-warning' role='alert'> You are still logged in. 
+		      								Please <a href='account.php'> click here </a> to proceed. </div>";
+
+		      			}
+
+		      			?>
+
+
+
+
+
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username"required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,7 +125,10 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	
+
+
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -51,11 +136,6 @@
 			</div>
 		</div>
 	</section>
-
-  <!--<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script> -->
 
 	</body>
 </html>
